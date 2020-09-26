43f5c17f8f18607cd6f80ef836e7d527280d8080
Thet Oo Aung <thetooaung07@gmail.com>
2020-09-27 00:09:56 +0630

    remove unuse imports

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,4 +1,4 @@
-import { Grid, Typography } from "@material-ui/core";
+import { Grid } from "@material-ui/core";
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import AboutCard from "./AboutCard";
