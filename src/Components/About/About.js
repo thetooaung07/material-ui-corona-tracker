195fa99338dee99c4d473ce67169763bfa3d953c
Thet Oo Aung <thetooaung07@gmail.com>
2020-10-05 21:41:25 +0630

    bug fix for background shrinking

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -5,16 +5,15 @@ import AboutCard from "./AboutCard";
 
 const useStyle = makeStyles((theme) => ({
   rootContainer: {
-    width: "70%",
     margin: "0 auto",
     // [theme.breakpoints.down("md")]: {
     //   height: "100vh"
     // },
     [theme.breakpoints.down("sm")]: {
-      height: "max-content"
+      height: "max-content",
     },
     [theme.breakpoints.up("sm")]: {
-      height: "100vh"
+      height: "100vh",
     },
   },
 }));
@@ -23,24 +22,23 @@ const About = () => {
   const classes = useStyle();
 
   return (
-    <div className={`About wrapper-sub ${classes.rootContainer}`}>
-    
-      <Grid container  >
-      
-        <Grid item xs={12} sm={6} md={6} lg={4}>
-          <AboutCard/>
+    <div className={`About wrapper-sub`}>
+      <div className={classes.rootContainer}>
+        <Grid container>
+          <Grid item xs={12} sm={6} md={6} lg={4}>
+            <AboutCard />
+          </Grid>
+          <Grid item xs={12} sm={6} md={6} lg={4}>
+            <AboutCard></AboutCard>
+          </Grid>
+          <Grid item xs={12} sm={6} md={6} lg={4}>
+            <AboutCard></AboutCard>
+          </Grid>
+          <Grid item xs={12} sm={6} md={6} lg={4}>
+            <AboutCard></AboutCard>
+          </Grid>
         </Grid>
-        <Grid item xs={12} sm={6} md={6} lg={4}>
-          <AboutCard></AboutCard>
-        </Grid>
-        <Grid item xs={12} sm={6} md={6} lg={4}>
-          <AboutCard></AboutCard>
-        </Grid>
-        <Grid item xs={12} sm={6} md={6} lg={4}>
-          <AboutCard></AboutCard>
-        </Grid>
-        
-      </Grid>
+      </div>
     </div>
   );
 };
